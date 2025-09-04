268f28c38f92315efba45de272031296ddd83b1b
Sandro Ciervo <sandro.ciervo@neolution.ch>
2025-09-04 22:01:15 +0200

    feat: patch prep for v0.2.1-beta.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+- Improved input handling in GetGreeting method by trimming whitespace from name parameter
+
 ## [0.2.0] - 2025-09-04
 
 ### Added
--- src/Neolution.ReleaseProcessTest/SampleService.cs
+++ src/Neolution.ReleaseProcessTest/SampleService.cs
@@ -18,7 +18,7 @@ public class SampleService
         try
         {
             ArgumentException.ThrowIfNullOrWhiteSpace(name);
-            return $"Hello, {name}! This is Neolution.ReleaseProcessTest library.";
+            return $"Hello, {name.Trim()}! This is Neolution.ReleaseProcessTest library.";
         }
         catch (ArgumentException ex)
         {
